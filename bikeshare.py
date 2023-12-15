2887403c5ff43fa2febb8eff1fe3d7dd66b552ef
tristenwallace <tri3sten@gmail.com>
2023-12-15 18:42:02 -0500

    Outline project deliverables

---
+++ bikeshare.py
@@ -0,0 +1,46 @@
+import time
+import pandas as pd
+import numpy as np
+
+#Features Needed:
+    #user input (city, month/day filter)
+    
+    #STAT Output:
+    #Popular times of travel (most common month, day, week)
+    #Popular stations (start, stop, most common route)
+    #Trip Duration (total travel time, avg. travel time)
+    #User Info (user type count, genser count, birth stats)
+    
+    #Rerun program if user wants
+
+city_data = {'chicago': 'chicago.csv',
+            'new york city': 'new_york_city.csv',
+            'washington': 'washington.csv'}
+
+def get_filters():
+    #TO DO
+    return
+
+def load_data():
+    #TO DO
+    return
+
+def time_stats():
+    #TO DO
+    return
+
+def station_stats():
+    #TO DO
+    return
+
+def trip_stats():
+    #TO DO
+    return
+
+def user_stats():
+    #TO DO
+    return
+
+if __name__ == "__main__":
+    #TO DO
+    print('nothing done yet')
