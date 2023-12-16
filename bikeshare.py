7f7343375e9d0e1c018eefd427aac6b85b6be31c
tristenwallace <tri3sten@gmail.com>
2023-12-15 20:32:51 -0500

    Feat: Load data function

--- bikeshare.py
+++ bikeshare.py
@@ -14,9 +14,9 @@ import re
     
     #Rerun program if user wants
 
-city_data = {'chicago': 'chicago.csv',
-            'new york city': 'new_york_city.csv',
-            'washington': 'washington.csv'}
+city_data = {'chicago': 'data/chicago.csv',
+            'nyc': 'data/new_york_city.csv',
+            'washington': 'data/washington.csv'}
 
 def get_filters():
     #Asks user to specify a city, month, and day to analyze.
@@ -62,9 +62,46 @@ def get_filters():
     
     return city, month, day
 
-def load_data():
-    #TO DO
-    return
+def load_data(city, month, day):
+    df = pd.read_csv(city_data[city], parse_dates=['Start Time', 'End Time'])
+
+    mask = pd.Series()
+    mask2 = pd.Series()
+    
+    if month == 'jan':
+        mask = (df['Start Time'] > '2017-01-01') & (df['Start Time'] < '2017-02-01')
+    elif month == 'feb':
+        mask = (df['Start Time'] > '2017-02-01') & (df['Start Time'] < '2017-03-01')
+    elif month == 'mar':
+        mask = (df['Start Time'] > '2017-03-01') & (df['Start Time'] < '2017-04-01')
+    elif month == 'apr':
+        mask = (df['Start Time'] > '2017-04-01') & (df['Start Time'] < '2017-05-01')
+    elif month == 'may':
+        mask = (df['Start Time'] > '2017-05-01') & (df['Start Time'] < '2017-06-01')
+    elif month == 'jun':
+        mask = (df['Start Time'] > '2017-06-01') & (df['Start Time'] < '2017-07-01')
+    elif month == 'all':
+        mask = (df['Start Time'] > '2017-01-01') & (df['Start Time'] < '2017-07-01')
+    
+    if day == 'm':
+        mask2 = (df['Start Time'].dt.dayofweek == 0)
+    elif day == 't':
+        mask2 = (df['Start Time'].dt.dayofweek == 1)
+    elif day == 'w':
+        mask2 = (df['Start Time'].dt.dayofweek == 2)
+    elif day == 'th':
+        mask2 = (df['Start Time'].dt.dayofweek == 3)
+    elif day == 'f':
+        mask2 = (df['Start Time'].dt.dayofweek == 4)
+    elif day == 's':
+        mask2 = (df['Start Time'].dt.dayofweek == 5)       
+    elif day == 'su':
+        mask2 = (df['Start Time'].dt.dayofweek == 6)    
+    elif day == 'all':
+        mask2 = (df['Start Time'].dt.dayofweek < 7)
+    
+        
+    return df.loc[mask].loc[mask2]
 
 def time_stats():
     #TO DO
@@ -83,5 +120,7 @@ def user_stats():
     return
 
 if __name__ == "__main__":
-    city, month, day = get_filters()
-    print('city:{} month:{} day:{}'.format(city, month, day))
+    #city, month, day = get_filters()
+    #df = load_data(city, month, day)
+    df = load_data(get_filters())
+    print(df['Start Time'].dt.dayofweek)
