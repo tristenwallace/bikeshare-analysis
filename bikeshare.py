31e53476ed0885331b201cb2906a3141ddf2975a
tristenwallace <tri3sten@gmail.com>
2023-12-16 18:29:49 -0500

    fix: refactor input and load data

--- bikeshare.py
+++ bikeshare.py
@@ -11,45 +11,56 @@ city_data = {'chicago': 'data/chicago.csv',
 
 def get_filters():
     #Asks user to specify a city, month, and day to analyze.
+    time_filter = ''
     city = ''
     month = ''
     day = ''
     
     #Ask for city
     while city == '':
-        try:
-            city = input("Which city do you want to analyze: Chicago, NYC, or Washington?\n").lower()
-            if not re.match('chicago|nyc|washington', city):
-                city = ''
-                raise ValueError("Invalid input. Please enter a valid city.\n")
+            city = input("Select a city filter: Chicago, NYC, or Washington?\n").lower()
+            if re.match('chicago|nyc|washington', city):
+                print("We'll provide data for {}\n".format(city.upper()))
             else:
-                print("We'll provide data for {}\n".format(city.upper())) 
-        except ValueError:
-            print("Invalid input. Please enter a valid city.\n")
+                city = ''
+                print("Invalid input. Please enter a valid city.\n")
+    
+    #Check for filters
+    while time_filter == '':
+        time_filter = input("Would you like to filter by month, day or none?\n").lower()
+        if re.match('month|day', time_filter):
+            print("We will filter by {}.\n".format(time_filter))
+        elif time_filter == 'none':
+            print("We won't filter the data.\n")
+        else:
+            time_filter = ''
+            print("Invalid input: Please enter month, day or none")
+    
+    #Filters
+    if time_filter == 'none':
+        month = 'all'
+        day = 'all'
     
     #Ask for month
-    while month == '':
-        try:
-            month = input("Which month do you want to filter by (or all): jan feb mar apr may jun all?\n").lower()
-            if not re.match('jan|feb|mar|apr|may|jun|all', month):
-                month = ''
-                raise ValueError("Invalid input. Please enter a valid month: jan feb mar apr may jun all\n")
-            else:
-                print("We'll filter by {}\n".format(month.upper())) 
-        except ValueError:
-            print("Invalid input. Please enter a valid month or all: jan feb mar apr may jun all\n")
+    elif time_filter == 'month':
+        day = 'all'
+        while month == '':
+                month = input("Which month? january february march april may june?\n").lower()
+                if not re.match('january|february|march|april|may|june', month):
+                    month = ''
+                    print("Invalid input. Please enter a valid month (january - june).\n")
     
-    #Ask for day
-    while day == '':
-        try:
-            day = input("Which day of the week do you want to filter by (or all): m t w th f s su all?\n").lower()
-            if not re.match('m|t|w|th|f|s|su|all', day):
-                day = ''
-                raise ValueError("Invalid input. Please enter a valid month (or all): m t w th f s su all\n")
-            else:
-                print("We'll filter by {}\n".format(day.upper())) 
-        except ValueError:
-            print("Invalid input. Please enter a valid month (or all): m t w th f s su all\n")
+    #Ask for day               
+    else:
+        month = 'all'    
+        while day == '':
+            try:
+                day = int(input("Which day? Please type as an integer (e.g., 0=Monday?\n"))
+                if day < 0 or day > 7:
+                    day = ''
+                    print("Invalid input. Please enter a valid month (or all): m t w th f s su all\n")
+            except ValueError:
+                print("Invalid input. Please enter a valid integer\n")
     
     print('-'*40)
     return city, month, day
@@ -58,46 +69,21 @@ def get_filters():
 def load_data(city, month, day):
     df = pd.read_csv(city_data[city], parse_dates=['Start Time', 'End Time'])
 
-    mask = pd.Series()
-    mask2 = pd.Series()
+    #Filter by month
+    df['month'] = df['Start Time'].dt.month
+    if month != 'all':
+        months = ['january', 'february', 'march', 'april', 'may', 'june']
+        df = df[df['month'] == months.index(month) + 1]
+
+    #Filter by day
+    df['day'] = df['Start Time'].dt.dayofweek
+    if day != 'all':
+        df = df[df['day'] == day]
+        
     
-    #create mask based on month filter
-    if month == 'jan':
-        mask = (df['Start Time'] > '2017-01-01') & (df['Start Time'] < '2017-02-01')
-    elif month == 'feb':
-        mask = (df['Start Time'] > '2017-02-01') & (df['Start Time'] < '2017-03-01')
-    elif month == 'mar':
-        mask = (df['Start Time'] > '2017-03-01') & (df['Start Time'] < '2017-04-01')
-    elif month == 'apr':
-        mask = (df['Start Time'] > '2017-04-01') & (df['Start Time'] < '2017-05-01')
-    elif month == 'may':
-        mask = (df['Start Time'] > '2017-05-01') & (df['Start Time'] < '2017-06-01')
-    elif month == 'jun':
-        mask = (df['Start Time'] > '2017-06-01') & (df['Start Time'] < '2017-07-01')
-    elif month == 'all':
-        mask = (df['Start Time'] > '2017-01-01') & (df['Start Time'] < '2017-07-01')
-    
-    #create mask based on day filter
-    if day == 'm':
-        mask2 = (df['Start Time'].dt.dayofweek == 0)
-    elif day == 't':
-        mask2 = (df['Start Time'].dt.dayofweek == 1)
-    elif day == 'w':
-        mask2 = (df['Start Time'].dt.dayofweek == 2)
-    elif day == 'th':
-        mask2 = (df['Start Time'].dt.dayofweek == 3)
-    elif day == 'f':
-        mask2 = (df['Start Time'].dt.dayofweek == 4)
-    elif day == 's':
-        mask2 = (df['Start Time'].dt.dayofweek == 5)       
-    elif day == 'su':
-        mask2 = (df['Start Time'].dt.dayofweek == 6)    
-    elif day == 'all':
-        mask2 = (df['Start Time'].dt.dayofweek < 7)
-    
-    #return filtered dataframe using masks
+    #return filtered dataframe
     print('-'*40)
-    return df.loc[mask].loc[mask2]
+    return df
 
 
 def time_stats(df, month, day):
@@ -112,7 +98,7 @@ def time_stats(df, month, day):
         print("The most common month is {}\n".format(top_month))
     
     # display the most common day of week
-    if day == 'all':
+    if day != 'all':
         top_dow = df['Start Time'].dt.dayofweek.value_counts().idxmax()
         print("The most common day of week is {}\n".format(top_dow))
     
@@ -155,12 +141,12 @@ def trip_stats(df):
     
     # display total travel time
     total = df['Trip Duration'].sum()
-    total_hours = total/(60*60)
+    total_hours = round(total/(60*60))
     print("Total Travel Time: {} hours".format(total_hours))
 
     # display mean travel time
     average_time = df['Trip Duration'].mean()
-    average_time_min = average_time / 60
+    average_time_min = round(average_time / 60)
     print("Mean Travel Time: {} minutes".format(average_time_min))
 
     
